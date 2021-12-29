7deb33a5f6ada3a3b55f891cef6f50032b1f27c0
Steven Samy <stevensivo27@gmail.com>
2021-12-29 09:43:30 +0200

    import a pydictionary and  build a function to search

--- dictionary/views.py
+++ dictionary/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-
+from PyDictionary import PyDictionary
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
+
+def word(request):
+    search = request.Get.get('search')
+    dictionary =  PyDictionary()
+    meaning = dictionary.meaning(search)
+    synonyms = dictionary.synonym(search)
+    antonyms = dictionary.antonyms(search)
+    return render(request, 'word.html') 
+
